d7e04396a1d793321f150301f9ec324a6b1e5692
Sebastian Lindfors <sl92cb@gmail.com>
2021-02-03 00:51:22 +0100

    1.3.5

--- Logic/Logic.lua
+++ Logic/Logic.lua
@@ -159,12 +159,12 @@ function NPC:SetBackground(kit)
 	local backgroundFrame = self.TalkBox.BackgroundFrame;
 	local overlay = backgroundFrame.OverlayKit;
 
-	if kit then
+	if kit and not L('disablebgtextures') then
 		local backgroundAtlas = GetFinalNameFromTextureKit('QuestBG-%s', kit)
 		local atlasInfo = C_Texture.GetAtlasInfo(backgroundAtlas)
 		if atlasInfo then
 			overlay:Show()
-			overlay:SetGradientAlpha('HORIZONTAL', 1, 1, 1, 0, 1, 1, 1, 1)
+			overlay:SetGradientAlpha('HORIZONTAL', 1, 1, 1, 0, 1, 1, 1, 0.5)
 
 			overlay:SetSize(atlasInfo.width, atlasInfo.height)
 			overlay:SetTexture(atlasInfo.file)
@@ -614,24 +614,21 @@ function TalkBox:OnDragStop()
 
 	point = point:sub(1,1) .. point:sub(2):lower()
 
+	-- convert center point to bottom
 	if ( point == 'Center' ) then
 		point = 'Bottom'
-
-		local cX = self:GetCenter()
-
-		x = ( cX * self:GetScale() ) - ( GetScreenWidth() / 2 ) 
+		-- calculate the horz offset from the center of the screen
+		x = ( self:GetCenter() * ImmersionFrame:GetScale() ) - ( GetScreenWidth() / 2 )
 		y = self:GetBottom()
-
 	end
+	
 	local isBottom = point == 'Bottom'
-
 	if isBottom then
 		y = y - (self.extraY or 0)
 	end
 
 	self:ClearAllPoints()
-	self.offsetX = x
-	self.offsetY = y
+	self.offsetX, self.offsetY = x, y
 
 	L.Set('boxpoint', point)
 	L.Set('boxoffsetX', x)
