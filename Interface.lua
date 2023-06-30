96b0f69629e1bcb915eff80cef9fafc862604868
Sebastian Lindfors <sl92cb@gmail.com>
2023-06-30 17:28:01 +0200

    Add initial support for 1.14.4

--- Interface.lua
+++ Interface.lua
@@ -3,7 +3,12 @@ local API = {}; ImmersionAPI = API;
 local IS_VANILLA = WOW_PROJECT_ID == WOW_PROJECT_CLASSIC or nil;
 local IS_RETAIL  = WOW_PROJECT_ID == WOW_PROJECT_MAINLINE or nil;
 local IS_CLASSIC = WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC or nil;
-local IS_WOW10   = select(4, GetBuildInfo()) >= 30401 or nil;
+local IS_WOW10   = (function() -- WoW10 == modern API
+	local version = select(4, GetBuildInfo())
+	if version >= 30401 or ( version >= 11404 and version <= 20000 ) then
+		return true
+	end
+end)();
 
 API.IsRetail = IS_RETAIL;
 API.IsWoW10  = IS_WOW10;
